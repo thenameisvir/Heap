1ca62cba3bb6cb1369fa326ce1f33b4c0958f5cb
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-06-27 12:29:16 +0530

    Update Kth largest and smallest element with best time and space complexity.cpp

--- Kth largest element with best time and space complexity.cpp
+++ Kth largest element with best time and space complexity.cpp
@@ -5,8 +5,9 @@ int kthsmallest(int arr[],int size,int k){
   priority_queue<int>pq;
   for(int i=0;i<k;i++){
     int e = arr[i];
-    pq.push(e);
+  pq.push(e);
   }
+
   for(int i=k;i<size;i++){
     int e = arr[i];
     if(e<pq.top()){
@@ -15,13 +16,34 @@ int kthsmallest(int arr[],int size,int k){
     }
   }
 
-  int ans = pq.top();
-  return ans;
+  int a = pq.top();
+  return a;
 }
-int main(){
-  int arr[10] = {3,9,4,6,5,8,7};
+int kthlargest(int arr[],int size,int k){
+priority_queue<int, vector<int>, greater<int>> pq;
+for(int i=0;i<k;i++){
+    int e = arr[i];
+  pq.push(e);
+  }
+
+  for(int i=k;i<size;i++){
+    int e = arr[i];
+    if(e>pq.top()){
+      pq.pop();
+      pq.push(e);
+    }
+  }
+
+  int a = pq.top();
+  return a;
+}
+int main()
+{
+  int arr[10] = {3,4,5,7,6,8,9};
   int size = 7;
   int k;
   cin>>k;
-  int answer = kthsmallest(arr,size,k);
-  cout<<answer;}
+  int answer = kthlargest(arr,size,k);
+  int answer2 = kthsmallest(arr,size,k);
+  cout<<answer<<endl<<answer2<<endl;
+}
